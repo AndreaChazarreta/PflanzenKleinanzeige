1940f5525826c417465ba27a53e950a2fcce6024
st178058 <st178058@stud.uni-stuttgart.de>
2024-05-08 13:38:35 +0200

    first commit

---
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/PflanzenKleinanzeigenApplication.java
@@ -0,0 +1,13 @@
+package com.sopra.pflanzenkleinanzeigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PflanzenKleinanzeigenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PflanzenKleinanzeigenApplication.class, args);
+    }
+
+}
