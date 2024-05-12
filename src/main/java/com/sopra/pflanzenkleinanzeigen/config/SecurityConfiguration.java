bcbbf536a31d1599cd5b80aa3dd5cb33819f364e
st178058 <st178058@stud.uni-stuttgart.de>
2024-05-12 18:10:49 +0200

    Spring Security einrichten

---
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package com.sopra.pflanzenkleinanzeigen.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        // define all URLs which should be accessible without login
+                        auth -> auth
+                                .requestMatchers("/register", "/login").permitAll()
+                                // all other URLs (except the ones above) require authentication too
+                                .anyRequest().authenticated())
+                // include CSRF token, which may be required while performing AJAX-requests
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("/console/**"))
+                // define the login-page, which is also accessible for everyone
+
+                .formLogin( formLogin -> formLogin
+                        .loginPage("/login").failureUrl("/login?error=true").permitAll()
+                        .defaultSuccessUrl("/", true)
+                        .usernameParameter("username")
+                        .passwordParameter("password"))
+                // everyone may logout
+
+                .logout(logout -> logout.permitAll()
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout"))
+
+                //Disables header security. This allows the use of the h2 console.
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web
+                .ignoring()
+                // gewähre immer Zugriff auf Dateien in den folgenden Ordnern
+                .requestMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+
+    /**
+     * Password-encoder Bean der Spring Security. Wird zum Verschlüsseln von Passwörtern benötigt.
+     *
+     * @return BCryptPasswordEncoder bean.
+     */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/sopra/pflanzenkleinanzeigen/service/UserService.java
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/service/UserService.java
@@ -3,12 +3,19 @@ package com.sopra.pflanzenkleinanzeigen.service;
 import com.sopra.pflanzenkleinanzeigen.entity.User;
 import com.sopra.pflanzenkleinanzeigen.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
@@ -20,4 +27,61 @@ public class UserService {
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
+
+    /**
+     * Sucht nach einem User mit einem bestimmten Usernamen.
+     *
+     * @param username der username.
+     * @return User-Objekt.
+     */
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Spring Security Authentication Methoden
+    ///////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Gibt den aktuell eingeloggten User zurück.
+     *
+     * @return User.
+     */
+    public User getCurrentUser() {
+        return getUserByUsername(((org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal()).getUsername());
+    }
+
+    /**
+     * Gibt das UserDetails Objekt des aktuell eingeloggten Users zurück.
+     *
+     * @return UserDetails Objekt der Spring Security.
+     */
+    public org.springframework.security.core.userdetails.User getCurrentUserDetails() {
+        return (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+    }
+
+    /**
+     * Überschreibt die Methode, welche für den Login der Spring Security benötigt wird..
+     *
+     * @param username the username des Benutzers.
+     * @return UserDetails Objekt des Spring Security Frameworks.
+     * @throws UsernameNotFoundException exception.
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)) {
+            throw new UsernameNotFoundException("Could not find the user for username " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isEnabled(), true, true, user.isEnabled(), grantedAuthorities);
+    }
+
+    //TODO: nochmal nachschauen was in diesen constructor rein gehört (also müssen wir "accountNonExpired" und "credentialsNonExpired"
+    // drin lassen??
+
 }
+
+
