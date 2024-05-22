a4d0a84602381ca3ad2d0d92bc41906986877810
Senadem03 <st178485@stud.uni-stuttgart.de>
2024-05-22 09:49:17 +0200

    Opened CareTip-Repository

---
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/repository/CareTipRepository.java
@@ -0,0 +1,9 @@
+package com.sopra.pflanzenkleinanzeigen.repository;
+
+import com.sopra.pflanzenkleinanzeigen.entity.CareTip;
+import com.sopra.pflanzenkleinanzeigen.entity.Rolle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CareTipRepository extends JpaRepository<Rolle, Integer> {
+
+}
