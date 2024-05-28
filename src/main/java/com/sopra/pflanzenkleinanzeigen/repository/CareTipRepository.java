14de5462b9ab8aba9d0b168ae8a3461a4a2d470c
st178058 <st178058@stud.uni-stuttgart.de>
2024-05-28 09:51:39 +0200

    Added Repository comments

--- src/main/java/com/sopra/pflanzenkleinanzeigen/repository/CareTipRepository.java
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/repository/CareTipRepository.java
@@ -3,6 +3,10 @@ package com.sopra.pflanzenkleinanzeigen.repository;
 import com.sopra.pflanzenkleinanzeigen.entity.CareTip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface provides methods to interact with the CareTip entity in the database.
+ * It extends JpaRepository which provides basic CRUD operations (Create, Read, Update, Delete) for the CareTip entity.
+ */
 public interface CareTipRepository extends JpaRepository<CareTip, Integer> {
 
 }
--- src/main/java/com/sopra/pflanzenkleinanzeigen/repository/UserRepository.java
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.sopra.pflanzenkleinanzeigen.repository;
 import com.sopra.pflanzenkleinanzeigen.entity.Benutzer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * This interface provides methods to interact with the User entity in the database.
+ * It extends JpaRepository which provides basic CRUD operations (Create, Read, Update, Delete) for the User entity.
+ */
 public interface UserRepository extends JpaRepository<Benutzer, Integer> {
 
     Benutzer findByUsername(String username);
