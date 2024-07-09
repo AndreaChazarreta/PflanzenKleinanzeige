459e4eca00c1a4560c435ae40abb144f9f0f8f4e
st178058 <st178058@stud.uni-stuttgart.de>
2024-07-09 15:57:11 +0200

    #23 Added new filters

--- src/main/java/com/sopra/pflanzenkleinanzeigen/service/PlantService.java
+++ src/main/java/com/sopra/pflanzenkleinanzeigen/service/PlantService.java
@@ -79,14 +79,16 @@ public class PlantService {
     public List<Plant> findPlantsByFilters(String name, BigDecimal minPrice, BigDecimal maxPrice,
                                            BigDecimal minHeight, BigDecimal maxHeight,
                                            Boolean potIncluded, List<String> categories,
-                                           Boolean excludeCurrentUser, Benutzer currentUser, String sortPrice) {
-        return plantRepository.findByFilters(name, minPrice, maxPrice, minHeight, maxHeight, potIncluded, categories, excludeCurrentUser, currentUser, sortPrice);
+                                           Boolean excludeCurrentUser, Benutzer currentUser,
+                                           Boolean fruits, Boolean airPurifying, Boolean toxicForPets, String sortPrice) {
+        return plantRepository.findByFilters(name, minPrice, maxPrice, minHeight, maxHeight, potIncluded, categories, excludeCurrentUser, currentUser, fruits,  airPurifying,  toxicForPets, sortPrice);
     }
 
     public List<Plant> findPlantsByFiltersWithoutCategory(String name, BigDecimal minPrice, BigDecimal maxPrice,
                                            BigDecimal minHeight, BigDecimal maxHeight, Boolean potIncluded,
-                                           Boolean excludeCurrentUser, Benutzer currentUser, String sortPrice) {
-        return plantRepository.findByFiltersWithoutCategory(name, minPrice, maxPrice, minHeight, maxHeight, potIncluded, excludeCurrentUser, currentUser, sortPrice);
+                                           Boolean excludeCurrentUser, Benutzer currentUser,
+                                           Boolean fruits, Boolean airPurifying, Boolean toxicForPets, String sortPrice) {
+        return plantRepository.findByFiltersWithoutCategory(name, minPrice, maxPrice, minHeight, maxHeight, potIncluded, excludeCurrentUser, currentUser, fruits,  airPurifying,  toxicForPets, sortPrice);
     }
 
     public BigDecimal findMaxPrice() {
